fe0e0a4533869295d5e230d2b7214247ec1baf89
Florent Dupont <Florent Dupont>
2016-11-09 19:01:43 +0100

    Loop improvement and add debug mode

--- main/main.ino
+++ main/main.ino
@@ -1,24 +1,25 @@
-/*  Clapclap_Lifx v0.2
+/*  Clapclap_Lifx v0.3
  *  
  *  Detect number of claps with some filters to avoid random detection problems
  *  
- *  Changelog : Give up interrupts method because of too much strange problems
- *              Re-written with common methods, works well
+ *  Changelog : Limit claps number to 5 to avoid constant detection bug
+ *              Finally use analogic output for better detection stability
+ *              Add Debug mode with Serial debugging messages, possibility to disable it
  *  
- *  TBD : Better filters
+ *  TBD : Add wireless communication
  *  
  *  Components : Microphone KY-038 || Buzzer || Blue LED (cause it is classy)
  *  
- *  Date : 10 June 2016
+ *  Date : 13 October 2016
  *  by Florent Dupont
  */
 
 
-// Pins assignation
+// --------------- Declarations ---------------
 int led = 3;
 
 int digitalMic = 2;
-int analogMic = A0;   // useless ?
+int analogMic = A0;   // usefull !
 
 int buzzer = 6;
 
@@ -29,14 +30,17 @@ double detectionTimeLimit = 500;    // Time before stopping between-clap listeni
 double clapDurationLimit = 1000;    // Time before stopping between-clap listening
 bool overtime = false;
 
+bool DEBUGMODE = true;
+
 int n = 0;
+// --------------------------------------------
 
 
-// --------------- Initialization ---------------
+// -------------- Initialization --------------
 void setup()
 {
   // Initializing Serial
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Setting up I/O
   pinMode(analogMic, INPUT);
@@ -45,61 +49,67 @@ void setup()
 
   pinMode(buzzer, OUTPUT);
 
-  Serial.println("--------------------- Debut du programme ---------------------");
+  printlnMessage("--------------------- Debut du programme ---------------------");
 }
+// --------------------------------------------
 
 
-// --------------- Loop function ---------------
+// --------------- Loop function --------------
 void loop()
 {
-  
-  if(clapClean())
+  if(clapClean())         // if clap is recognized
   {
-    n = clapCounter(1);
+    n = clapCounter(1);   // begin clap count
 
-    Serial.print("Valeur de n : ");
-    Serial.println(n);
+    printMessage("Valeur de n : ");
+    printlnMessage(n);
   }
 
-  if(!overtime)
+  
+  if(!overtime)       // if no overtime detection
   {
-    if(n == 2)
+    if(n == 3 && n < 5)
     {
       buzzOk();
-    }
 
-    for(n ; n>0 ; n--)
+      for(n ; n>0 ; n--)    // LED blinking
+      {
+        analogWrite(led, 90);
+        delay(100);
+        analogWrite(led, 0);
+        delay(100);
+      }
+    }
+    else if(n >= 5)
     {
-      analogWrite(led, 90);
-      delay(100);
-      analogWrite(led, 0);
-      delay(100);
+      buzzNotOk();
     }
   }
-  else
+  else                // if overtime detection
   {
-    Serial.println("OVERTIME !");
-    analogWrite(led, 90);
-    delay(clapDurationLimit);
-    analogWrite(led, 0);
+    printlnMessage("OVERTIME !");
+    buzzNotOk();
   }
 
   // reset
   overtime = false;
-  n == 0;
+  n = 0;
 }
+// --------------------------------------------
 
 
-// --------------- Other functions ---------------
+// --------------- Function library ---------------
 int clapCounter(int m)
 {
-  Serial.println(m);
+  printlnMessage(m);
   double detectionTime = millis();
   while(millis() - detectionTime < detectionTimeLimit)
   {
     if(clapClean())
     {
       m++;
+      if(m >= 5)
+        return m;
       m = clapCounter(m);
     }
   }
@@ -115,25 +125,39 @@ void buzzOk()
   delay(400);
 }
 
+void buzzNotOk()
+{
+  analogWrite(led, 90);
+  tone(buzzer, 300, 100);
+  delay(200);
+  tone(buzzer, 300, 100);
+  delay(200);
+  tone(buzzer, 300, 100);
+  delay(200);
+  analogWrite(led, 0);
+}
+
 
 int clapClean()
 {
-  if(digitalRead(2))
+  if(getMicroValue())
   {
-    double beginTime = millis();  // temps de détection
+    double beginTime = millis();      // temps de détection
     int mode = 1;
     while(mode == 1)
     {
       double beginTimeInside = millis();  
-      while(millis() - beginTimeInside <= 50)
+      
+      // overtime detection algorithm
+      while(millis() - beginTimeInside <= 50)           // while sound is detected within 50 milliseconds
       {
-        if(millis() - beginTime > clapDurationLimit)
+        if(millis() - beginTime > clapDurationLimit)    // if it is during for more than 500 milliseconds, stop clap detection
         {
           mode = 0;
           overtime = true;
           return false;
         }
-        if(digitalRead(2))
+        if(getMicroValue())
         {
           mode = 1;
           break;
@@ -145,4 +169,28 @@ int clapClean()
   }
   else
     return false;
+}
+
+int getMicroValue()
+{
+  printlnMessage(analogRead(A0));
+  return (analogRead(A0) > 545);
+}
+
+void printMessage(String message)
+{
+  if(DEBUGMODE)
+    Serial.print(message);
+}
+
+void printlnMessage(String message)
+{
+  if(DEBUGMODE)
+    Serial.println(message);
+}
+
+void printlnMessage(int message)
+{
+  if(DEBUGMODE)
+    Serial.println(message);
 }
